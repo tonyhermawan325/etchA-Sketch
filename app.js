15725a9c063881c7c4330a17c2bcb0464080cdff
hermawanTony <tonyhermawan325@gmail.com>
2021-10-29 21:20:04 +0700

    basic function of creating div is done but still try to find a way to solve mouse hover function

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
-let divNumber = 4;
+let divNumber = 20;
 const containter = document.querySelector("#Container");
 
 for(let row = 0; row < divNumber ; row++){
     let div;
     let divRow;
+    let paragraph;
     divRow = document.createElement('div');
+    divRow.setAttribute("data-row", `${row}`);
     divRow.classList.add("divRow");
     containter.appendChild(divRow);
-    console.log(divRow);
+    console.log(`${row}`);
+    for(let col = 0; col < divNumber; col++){
+        paragraph = document.createElement('p');
+        paragraph.textContent = `${row}, ${col}`;
+        div = document.createElement('div');
+        div.setAttribute("data-rowCol", `${row},${col}`);
+        div.classList.add("divElement");
+        divRow.appendChild(div);
+        }
+    
 }
 
+
+const divEl = document.querySelectorAll(".divElement");
+divEl.forEach(function(){
+    divEl.addEventListener("mouseover", function(event){
+        event.target.style.color = "crimson";
+    })
+})
+
+
+
+
 /**
-    for(let col = 0; row < divNumber; col++){
-    div = document.createElement('div');
-    div.
-    }
+    
  * */
